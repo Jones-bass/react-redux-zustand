0e685c2dd33c9caca4b7cf62da40e0ed46e0f395
jones Silva <jonesbass.tb@gmail.com>
2023-08-04 15:37:07 -0300

    Estrutura

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
+      <p>Click on the Vite and React logos to learn more</p>
+    </div>
+  )
+}
